90caf5f8ab9bfa47440326fb3629bc1603b82c0e
JonhXQ <LopezJuarezJese.123@hotmail.com>
2023-04-24 20:46:49 -0600

    Versión 11

---
+++ CLASE(2_2)19_04_23/ACTIVIDAD 1/JS/paridad.js
@@ -0,0 +1,7 @@
+/* Solicitar al usuario un número, y determinar
+si es divisible entre dos o no. Mostrando al usuario 
+un mensaje de “x número es divisible entre 2” o “x núm */
+
+/* let Num = prompt("Ingrese número");
+if (Num%2==0) {alert("El número ingresado es par")} 
+else {alert("El número ingresado es impar")} */
