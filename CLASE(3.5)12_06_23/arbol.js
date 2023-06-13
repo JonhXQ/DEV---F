f46af9a6567ff2d2fbb8d914c7b6623e18296551
JonhXQ <LopezJuarezJese.123@hotmail.com>
2023-06-12 21:10:38 -0600

    Versión 14

---
+++ CLASE(3.5)12_06_23/arbol.js
@@ -0,0 +1,72 @@
+class Nodo{
+    constructor(valor){
+        this.valor = valor;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
+
+class ArbolBinario{
+    constructor(){
+        this.raiz = null;
+    }
+
+    insertar(valor){
+        if(this.raiz === null){
+            let nuevoNodo = new Nodo(valor);
+            this.raiz = nuevoNodo;
+        }else{
+            let nodoActual = this.raiz;
+            while(true){
+                if(valor < nodoActual.valor){
+                    if(nodoActual.izquierdo === null){
+                        let nuevoNodo = new Nodo(valor);
+                        nodoActual.izquierdo = nuevoNodo;
+                        return;
+                    }else{
+                        nodoActual = nodoActual.izquierdo;
+                    }
+                }else{
+                    if(nodoActual.derecho === null){
+                        let nuevoNodo = new Nodo(valor);
+                        nodoActual.derecho = nuevoNodo;
+                        return;
+                    }else{
+                        nodoActual = nodoActual.derecho;
+                    }
+                }
+            }
+        }
+    }
+
+    buscar(valor){
+        let nodoActual = this.raiz;
+        if(nodoActual === null){
+            return false;
+        }
+
+        while(nodoActual !== null){
+            if(nodoActual.valor === valor){
+                return true;
+            }
+            if(valor < nodoActual.valor){
+                nodoActual = nodoActual.izquierdo;
+            }else{
+                nodoActual = nodoActual.derecho;
+            }
+        }
+
+        return false;
+    }
+}
+
+let arbolBinario = new ArbolBinario();
+arbolBinario.insertar(5);
+arbolBinario.insertar(3);
+arbolBinario.insertar(4);
+arbolBinario.insertar(7);
+arbolBinario.insertar(6);
+arbolBinario.insertar(7);
+
+let valorBuscar = 2;
+console.log(`El valor ${valorBuscar} existe: ${arbolBinario.buscar(valorBuscar)}`);
